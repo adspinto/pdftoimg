f9a046a7c569837cde55dc2078338fdec9910212
Andre Pinto <andre.sousa@am4.com.br>
2020-07-17 17:22:21 -0300

    code push fix

--- App.js
+++ App.js
@@ -4,5 +4,12 @@ import AppContainer from './src'
 import codePush from "react-native-code-push";
 
 const App = () => <AppContainer />
-export default  codePush(App);
+
+const codePushOptions = {
+    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+    installMode: codePush.InstallMode.ON_NEXT_RESTART
+  }
+
+
+export default  codePush(codePushOptions)(App);
       
